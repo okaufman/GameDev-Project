367cd8da8e65a0c78d1453f7b8e4de11910cc521
Olivia Kaufmann <olivia.kaufmann@stud.hslu.ch>
2018-12-01 20:45:18 +0100

    added grunt

---
+++ Assets/plattform/TowerEnemy/Grunt/GruntBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GruntBehaviour : MonoBehaviour {
+    public float speed = 4.0F;
+    public Rigidbody myBody;
+    public Transform myTransform;
+    public Transform FirePoint;
+   // private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+       myBody= this.GetComponent <Rigidbody>();
+       myTransform = this.transform;
+    
+        
+		
+	}
+	
+	// Update is called before Rendering
+	void FixedUpdate () {
+
+        
+
+        //Always move forward
+        Vector2 myVel = myBody.velocity;
+        myVel.x = myTransform.right.x *speed;
+        myBody.velocity = myVel;
+        
+		
+	}
+
+    //private void OnTriggerEnter()
+    
+    void OnCollisionEnter(Collision collision)
+    {
+
+        print(collision.contacts);
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
+        switchDirection();
+        
+        
+    }
+
+    void switchDirection()
+    {   
+        //save current rotation
+        Vector3 currRot = transform.eulerAngles;
+        //change rotation
+        currRot.y += 180;
+        //assign new Rotation to object
+        transform.eulerAngles = currRot;
+        
+    }
+
+    
+}
--- Assets/plattform/TowerEnemy/TEnemyGun.cs
+++ Assets/plattform/TowerEnemy/TEnemyGun.cs
@@ -2,37 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TEnemyGun : MonoBehaviour {
-    public Transform firePoint;
+public class TEnemyGun : MonoBehaviour
+{
+    public Transform firePointRight;
+    public Transform firePointLeft;
     public Rigidbody smallBullet;
     public Transform Player;
-    public int fireRate = 200;
+    public float nextFire = 0.0F;
+    public float fireRate = 1;
+    public int attackRange = 2;
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         int Health = 100;
         int Resilience = 0;
-	}
-	// Update is called once per frame
-	void Update () {
-        AimAtPlayer();
-        StartCoroutine(Shoot());
+
+
     }
-    IEnumerator Shoot()
+    // Update is called once per frame
+    void Update()
     {
-        print("start");
+
+        if ((Vector3.Distance(firePointLeft.position, Player.position) < attackRange))
+        {
+            if (Time.time > nextFire)
+            {
+                if (Vector3.Distance(Player.position, firePointRight.position) > Vector3.Distance(Player.position, firePointLeft.position))
+                {
+
+                    Shoot(firePointLeft);
+                    nextFire = Time.time + fireRate;
+                }
+                else
+                {
+                    Shoot(firePointRight); 
+                    nextFire = Time.time + fireRate;
+                }
+            }
+        }
+
+    }
+    void Shoot(Transform firePoint)
+    {
+        
+        AimAtPlayer(firePoint);
+        
         Rigidbody Bullet = Instantiate(smallBullet, firePoint.position + firePoint.forward, firePoint.rotation);
         Bullet.AddForce(firePoint.forward * 200);
-        //wait a couple seconds before firing again
-        yield return new WaitForSeconds(fireRate);
-        print("fired");
+        //print("fired");
     }
     //TODO implement aimatplayer
-    void AimAtPlayer()
+    void AimAtPlayer(Transform firePoint)
     {
         firePoint.LookAt(Player);
     }
+
+    
 
 
 
