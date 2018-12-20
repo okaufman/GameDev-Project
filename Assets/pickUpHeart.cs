1a5f4c9083fecfc0317ce0493a1bd59d7ccd4587
Elia Grifo <elia.grifo@stud.hslu.ch>
2018-12-20 15:15:35 +0100

    Bonusgegenstaende Leben und Zeit hinzugefuegt

---
+++ Assets/pickUpHeart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpHeart : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "redPlayer" || collision.gameObject.tag == "bluePlayer") {
+            Destroy(gameObject);
+            UIHealth.health += 50;
+        }
+    }
+}
